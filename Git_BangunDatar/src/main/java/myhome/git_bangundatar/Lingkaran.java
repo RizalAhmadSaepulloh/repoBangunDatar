4543f4354c683f704adf35d816a8c10f7649c374
SherrifRaynor <sherrifraynor2004@gmail.com>
2023-12-21 09:14:54 +0700

    perhitungan Lingkaran

--- Git_BangunDatar/src/main/java/myhome/git_bangundatar/Lingkaran.java
+++ Git_BangunDatar/src/main/java/myhome/git_bangundatar/Lingkaran.java
@@ -8,6 +8,25 @@ package myhome.git_bangundatar;
  *
  * @author rizal
  */
-public class Lingkaran extends {
-    
+public class Lingkaran extends BangunDatar {
+
+    private double jariJari;
+
+    // Konstruktor untuk mengatur jari-jari lingkaran
+    public Lingkaran(double jariJari) {
+        this.jariJari = jariJari;
+    }
+
+    @Override
+    public double hitungLuas() {
+        return Math.PI * Math.pow(jariJari, 2);
+        
+    }
+
+    @Override
+    public double hitungkeliling() {
+        return 2 * Math.PI * jariJari;
+
+    }
+
 }
